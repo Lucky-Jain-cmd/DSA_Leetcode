dfd255951ad008b3c7dfa00e54b513c91b6d6fd8
Lucky Jain <lucky.jain_cs23@gla.ac.in>
2025-08-01 22:24:47 +0530

    Time: 1 ms (99.54%), Space: 57.1 MB (56.07%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,14 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-      int sum = nums[0];
-        int maxi = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            // Either continue the subarray or start fresh from current element
-            sum = Math.max(nums[i], sum + nums[i]);
-            maxi = Math.max(maxi, sum);
+        int sum=nums[0];
+        int maxi=nums[0];
+        for(int i=1;i<nums.length;i++){
+            sum = Math.max(nums[i],sum+nums[i]);
+            maxi=Math.max(sum,maxi);
         }
-
         return maxi;
     }
 }
