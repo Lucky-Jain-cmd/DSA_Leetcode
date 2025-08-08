b74539ee9a4c87bc1d471dfc4fcd43de56583a3c
Lucky Jain <lucky.jain_cs23@gla.ac.in>
2025-08-08 22:26:20 +0530

    Time: 13 ms (89.16%), Space: 58.3 MB (61.82%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,11 +2,10 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> hs = new HashSet<>();
         for(int i : nums){
+            if(hs.contains(i)) return true;
             hs.add(i);
         }
-        if(hs.size()==nums.length){
-            return false;
-        }
-        return true;
+       
+        return false;
     }
 }
