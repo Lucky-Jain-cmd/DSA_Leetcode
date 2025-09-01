dde65fb063c47d97d023927b2af47ec75f7a597b
Lucky Jain <lucky.jain_cs23@gla.ac.in>
2025-09-01 23:03:42 +0530

    Time: 0 ms (100%), Space: 40.5 MB (51.57%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.java
+++ 2083-three-divisors/2083-three-divisors.java
@@ -1,9 +1,18 @@
 class Solution {
     public boolean isThree(int n) {
-        int c=0;
-        for(int i=1;i<=n;i++){
-            if(n%i==0) c++;
-        }
-     return c==3 ? true:false;
+    //     int c=0;
+    //     for(int i=1;i<=n;i++){
+    //         if(n%i==0) c++;
+    //     }
+    //  return c==3 ? true:false;
+
+    //               More OPTIMIZED
+    int sqrt = (int)Math.sqrt(n);
+    if(sqrt*sqrt!=n) return false;
+    if(sqrt<2) return false;
+    for(int i=2;i<=Math.sqrt(sqrt);i++){
+        if(sqrt%i==0) return false;
+    }
+    return true;
     }
 }
