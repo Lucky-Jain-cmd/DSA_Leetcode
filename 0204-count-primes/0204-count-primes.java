bceca7804bd90667ff315916ae2990cab607c635
Lucky Jain <lucky.jain_cs23@gla.ac.in>
2025-09-26 21:39:11 +0530

    Time: 102 ms (36.79%), Space: 64.3 MB (11.1%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,20 +1,39 @@
+// class Solution {
+//     public int countPrimes(int n) {
+//         if(n==0 || n==1) return 0;
+//         boolean isprime[] = new boolean[n];
+//         Arrays.fill(isprime,true);
+//         isprime[0]=false;
+
+//         isprime[1]=false;
+//         for(int i=2;i<=Math.sqrt(n);i++){
+//             for(int j=i*i;j<n;j +=i){
+//                 isprime[j]=false;
+//             }
+//         }
+//         int c=0;
+//         for(int i=0;i<n;i++){
+//             if(isprime[i]==true) c++;
+//         }
+//         return c;
+//     }
+// }
 class Solution {
     public int countPrimes(int n) {
-        if(n==0 || n==1) return 0;
-        boolean isprime[] = new boolean[n];
-        Arrays.fill(isprime,true);
-        isprime[0]=false;
-
-        isprime[1]=false;
-        for(int i=2;i<=Math.sqrt(n);i++){
-            for(int j=i*i;j<n;j +=i){
-                isprime[j]=false;
+ 
+        int arr[] = new int[n];
+        for(int i=2; i*i<n; i++){
+            if(arr[i]==0){
+                for(int j=i*i; j<n; j=j+i){
+                    arr[j]=1;
+                }
             }
         }
-        int c=0;
-        for(int i=0;i<n;i++){
-            if(isprime[i]==true) c++;
+        int count = 0;
+        for(int i=2; i<n; i++){
+            if(arr[i]==0) count++;
         }
-        return c;
+        return count;
     }
 }
+
