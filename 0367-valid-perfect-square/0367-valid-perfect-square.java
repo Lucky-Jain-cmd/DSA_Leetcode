f02ba7c303078073f3ef799bf2398ea9c0df19ed
Lucky Jain <lucky.jain_cs23@gla.ac.in>
2025-09-27 22:40:31 +0530

    Time: 0 ms (100%), Space: 40.3 MB (83.93%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if (num < 2) return true;
-
-        long left = 2, right = num / 2;
-        while (left <= right) {
-            long mid = left + (right - left) / 2;
-            long square = mid * mid;
-
-            if (square == num) return true;
-            else if (square < num) left = mid + 1;
-            else right = mid - 1;
+        if(num==1) return true;
+        long start=1,end=num/2;
+        while(start<=end){
+            long mid=start+(((long)(end-start))/2);
+            if(mid*mid==num) return true;
+            else if((mid*mid)>num){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
         }
         return false;
     }
